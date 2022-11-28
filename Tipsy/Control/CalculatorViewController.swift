f06185e25dfca2f4a72a0934804c8b93493a2463
PhilippEdrichCodes <philipp@edrich.codes>
2022-11-28 11:58:21 +0100

    IBOutlets fixed

--- Tipsy/Control/CalculatorViewController.swift
+++ Tipsy/Control/CalculatorViewController.swift
@@ -15,13 +15,23 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPercentButton: UIButton!
     @IBOutlet weak var twentyPercentButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip: Double = 0.10
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        
         zeroPercentButton.isSelected = false
         tenPercentButton.isSelected = false
         twentyPercentButton.isSelected = false
         
         sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle ?? "15%"
+        let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign) ?? 0.15
+        
+        tip = buttonTitleAsANumber / 100
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
